ab30c0e0de0134e2712d1608ab9b0221035b2736
woohyunbae <bwh1224@gmail.com>
2024-01-26 16:39:23 +0900

    feat: 텍스트 변경

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        새로운 PR 이 올라왔습니다~~!
         {process.env.REACT_APP_ENV}
       </header>
     </div>
