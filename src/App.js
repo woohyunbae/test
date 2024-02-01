0ff9f156d85b975dfd51ce9827594d1e8143ca66
woohyunbae <bwh1224@gmail.com>
2024-02-01 17:39:29 +0900

    feat: pr test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          PR TEST
         </a>
         {process.env.REACT_APP_ENV}
       </header>
